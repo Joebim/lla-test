d83eac8fa03cd433431224ef23823643cc653df0
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-08-07 22:27:25 +0100

    feat: articles page initialised

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "~/components/footer/Footer";
+import FirstHero from "./_components_/FirstHero";
+import FourthHero from "./_components_/FourthHero";
+
+const AboutPage = () => {
+  return (
+    <section className="w-full">
+      <FirstHero />
+      <FourthHero />
+      <Footer />
+    </section>
+  );
+};
+export default AboutPage;
