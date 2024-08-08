d68178d1753a81906ec150f840d1f1c2402436f7
anniedevkiller <erisannieannie@gmail.com>
2024-08-08 20:05:39 +0100

    feat: article continue

--- src/app/(other-routes)/blog/articles/[slug]/page.tsx
+++ src/app/(other-routes)/blog/articles/[slug]/page.tsx
@@ -4,9 +4,6 @@ import PhraseList from "../_components_/PhraseList";
 import SecondHero from "../_components_/SecondHero";
 
 export default function ArticlePage({ params }: { params: { slug: string } }) {
-  // Here you would typically fetch the article data based on the slug
-  // For now, we'll just use the slug in a console.log
-  // eslint-disable-next-line no-console
   console.log(`Article slug: ${params.slug}`);
 
   return (
