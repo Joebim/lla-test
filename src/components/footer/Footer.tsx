2a0465a73b510fdf1464630bf57fbe9ecb229e9c
Angel Afube <angel@zestpayment.com>
2024-08-08 16:05:58 +0100

    edit the mobile view of the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,50 +5,50 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-secondary-100 py-12 text-white">
       <div className="w-full">
-        <div className="flex w-full flex-wrap justify-between px-[5%] pb-16">
-          <div className="mb-6 w-full md:mb-0 md:w-[30%]">
+        <div className="flex w-full flex-wrap justify-center lg:justify-between px-[5%] pb-16">
+          <div className="mb-6 w-full md:mb-0 md:w-[30%] flex flex-col gap-4 items-center md:items-start ">
             <LogoFooter />
-            <p className="ml-0 mt-3">Your go-to language learning app.</p>
+            <p className="ml-0 text-sm md:text-xl text-transparent-white-75 font-normal ">Your go-to language learning app.</p>
           </div>
-          <div className="flex justify-between gap-5 md:w-[60%]">
+          <div className="flex flex-col md:flex-row justify-between gap-5 md:w-[60%] ">
             <div className="mb-6 flex w-full justify-end md:mb-0 md:w-1/3">
               <div>
-                <h3 className="mb-2 text-gray-400">Product</h3>
+                <h3 className="mb-2 text-base md:text-xl font-medium text-secondary-80">Product</h3>
                 <ul>
                   <li>
-                    <a href="#" className="text-white">
+                    <a href="#" className="text-sm md:text-xl text-white">
                       Features
                     </a>
                   </li>
                   <li>
-                    <a href="/how-it-works" className="text-white">
+                    <a href="/how-it-works" className="text-sm md:text-xl text-white">
                       How It Works
                     </a>
                   </li>
                 </ul>
               </div>
             </div>
-            <div className="mb-6 flex w-full justify-end md:mb-0 md:w-1/3">
+            <div className="mb-6 flex w-full justify-start  md:justify-end md:mb-0 md:w-1/3">
               <div>
-                <h3 className="mb-2 text-gray-400">Company</h3>
+                <h3 className="mb-2 text-base md:text-xl font-medium text-secondary-80">Company</h3>
                 <ul>
                   <li>
-                    <a href="/about-us" className="text-white">
+                    <a href="/about-us" className="text-sm md:text-xl text-white">
                       About Us
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="text-white">
+                    <a href="#" className="text-sm md:text-xl text-white">
                       Contact Us
                     </a>
                   </li>
                   <li>
-                    <a href="/faqs" className="text-white">
+                    <a href="/faqs" className="text-sm md:text-xl text-white">
                       FAQs
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="text-white">
+                    <a href="#" className="text-sm md:text-xl text-white">
                       Blog
                     </a>
                   </li>
@@ -57,20 +57,20 @@ const Footer: React.FC = () => {
             </div>
             <div className="mb-6 flex w-full justify-end md:mb-0 md:w-1/3">
               <div>
-                <h3 className="mb-2 text-gray-400">Legal</h3>
+                <h3 className="mb-2 text-base md:text-xl font-medium text-secondary-80 ">Legal</h3>
                 <ul>
                   <li>
-                    <a href="/terms-of-use" className="text-white">
+                    <a href="/terms-of-use" className="text-sm md:text-xl text-white">
                       Terms of Use
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="text-white">
+                    <a href="#" className="text-sm md:text-xl text-white">
                       Privacy Policy
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="text-white">
+                    <a href="#" className="text-sm md:text-xl text-white">
                       Legal Centre
                     </a>
                   </li>
