b063c1a1a07e018622a4efbc705a0aed4680e011
Mary <101870828+Chioma227@users.noreply.github.com>
2024-08-09 06:59:42 +0100

    pull commit

--- src/app/(other-routes)/blog/articles/_components_/FirstHero.tsx
+++ src/app/(other-routes)/blog/articles/_components_/FirstHero.tsx
@@ -1,29 +1,37 @@
 import Image from "next/image";
 
-//import LightNav from "~/components/light-navbar/LightNav";
-import blog from "../../../../../../public/blog/Image.svg";
+interface heroProperties {
+  title: string;
+  date: string;
+  genre: string;
+  readDuration: number;
+  imageSrc: string;
+}
 
-const FirstHero = () => {
+const FirstHero = ({
+  title,
+  date,
+  readDuration,
+  imageSrc,
+  genre,
+}: heroProperties) => {
   return (
     <header className="flex w-full flex-col items-center gap-[10px] bg-secondary-120 p-[12px] lg:p-[40px]">
       <div className="flex w-full flex-col gap-[48px] border-transparent-white-15 bg-white p-[12px] md:pb-[48px] lg:gap-[64px] lg:border-[8px] lg:border-solid">
-        {/*<LightNav className="relative mx-auto w-full max-w-[1654px] bg-white" />*/}
         <div className="px-[13%] text-left">
-          <h1 className="text-2xl font-bold tracking-[0.06em]">
-            Useful Spanish Phrases To Learn
-          </h1>
+          <h1 className="text-2xl font-bold tracking-[0.06em]">{title}</h1>
           <div className="mt-[12px] flex flex-col items-center gap-4 lg:flex-row">
-            <h6 className="text-sm">Christian Wang</h6>
+            {genre && <h6 className="text-sm">{genre}</h6>}
             <div className="h-2 w-2 rounded-full bg-primary-100"></div>
-            <h6 className="text-sm">August 6, 2024</h6>
+            <h6 className="text-sm">{date}</h6>
             <div className="h-2 w-2 rounded-full bg-primary-100"></div>
-            <h6 className="text-sm">6 min read</h6>
+            <h6 className="text-sm">{readDuration} min read</h6>
           </div>
         </div>
         <div className="mt-6 flex w-full justify-center px-[13%]">
           <div className="w-full">
             <Image
-              src={blog}
+              src={imageSrc}
               layout="responsive"
               width={1000}
               height={800}
