85be5d3a556edcc279b99a26de587f48e8011428
Mary <101870828+Chioma227@users.noreply.github.com>
2024-08-09 13:30:53 +0100

    feat: new commit

--- src/app/(other-routes)/blog/page.tsx
+++ src/app/(other-routes)/blog/page.tsx
@@ -1,43 +1,6 @@
 import Link from "next/link";
 
-interface BlogCard {
-  title: string;
-  date: string;
-  duration: string;
-  image: string;
-  slug: string;
-}
-
-const blogData: BlogCard[] = [
-  {
-    title: "Useful Spanish Phrases To Learn",
-    date: "August 6, 2024",
-    duration: "6 min read",
-    image: "/blog/blog-img-1.png",
-    slug: "useful-spanish-phrases",
-  },
-  {
-    title: "Benefits of AI In Language Learning",
-    date: "August 6, 2024",
-    duration: "6 min read",
-    image: "/blog/blog-img-2.png",
-    slug: "benefits-of-ai-in-language-learning",
-  },
-  {
-    title: "The Secret To Learning A Language In 2 Weeks",
-    date: "August 6, 2024",
-    duration: "6 min read",
-    image: "/blog/blog-img-3.png",
-    slug: "secret-to-learning-language-in-2-weeks",
-  },
-  {
-    title: "The Truth About Foreign Language Learning Apps",
-    date: "August 6, 2024",
-    duration: "6 min read",
-    image: "/blog/blog-img-4.png",
-    slug: "truth-about-foreign-language-learning-apps",
-  },
-];
+import blogArticlesData from "./articles/data";
 
 export default function Blog() {
   return (
@@ -48,20 +11,24 @@ export default function Blog() {
         </h1>
       </header>
       <div className="grid gap-[40px] px-[15px] py-[40px] sm:grid-cols-2 lg:grid-cols-3 lg:px-[80px] lg:py-[80px]">
-        {blogData.map((data, index) => (
-          <Link href={`/articles/${data.slug}`} key={index} className="block">
+        {blogArticlesData.map((data, index) => (
+          <Link
+            href={`/blog/articles/${data.slug}`}
+            key={index}
+            className="block"
+          >
             <div className="cursor-pointer transition-transform hover:scale-105">
               <div
                 className="mb-[32px] h-[264px] bg-cover"
-                style={{ backgroundImage: `url(${data.image})` }}
+                style={{ backgroundImage: `url(${data.properties.blogImage})` }}
               ></div>
               <h2 className="text-secoondary-120 mb-[24px] font-semibold lg:text-[20px]">
-                {data.title}
+                {data.name}
               </h2>
               <div className="flex items-center gap-[10px] text-[14px] lg:text-[16px]">
-                <p>{data.date}</p>
+                <p>{data.properties.date}</p>
                 <div className="h-[6.67px] w-[6.67px] rounded-full bg-primary-110"></div>
-                <p>{data.duration}</p>
+                <p>{data.properties.readDuration} min read</p>
               </div>
             </div>
           </Link>
