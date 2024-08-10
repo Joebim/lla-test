d1b61e7eb59463b67446d9e474e93a046aa1e23e
Frank Ezene <ezenefrankc@gmail.com>
2024-08-10 15:42:01 +0100

    feat: create src/app/dashboard/(admin)/admin/quest-management/types/index.ts and media-library-store.ts

---
+++ src/store/media-library-store.ts
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+
+import { MediaState } from "~/app/dashboard/(admin)/admin/quest-management/types";
+
+const useMediaStore = create<MediaState>((set) => ({
+  collections: [],
+  addCollection: (collection) =>
+    set((state) => ({ collections: [...state.collections, collection] })),
+  deleteCollection: (id) =>
+    set((state) => ({
+      collections: state.collections.filter(
+        (collection) => collection.id !== id,
+      ),
+    })),
+  editCollection: (id, updatedCollection) =>
+    set((state) => ({
+      collections: state.collections.map((collection) =>
+        collection.id === id ? updatedCollection : collection,
+      ),
+    })),
+  clearAllCollections: () => set({ collections: [] }),
+  addImageToCollection: (image, collectionId = "default") =>
+    set((state) => {
+      const collectionsCopy = [...state.collections];
+      const collectionIndex = collectionsCopy.findIndex(
+        (c) => c.id === collectionId,
+      );
+      if (collectionIndex === -1) {
+        collectionsCopy.push({
+          id: collectionId,
+          type: "backgrounds",
+          title: "Collection 01",
+          images: [image],
+        });
+      } else {
+        collectionsCopy[collectionIndex].images.push(image);
+      }
+      return { collections: collectionsCopy };
+    }),
+  deleteImageFromCollection: (collectionId, imageId) =>
+    set((state) => ({
+      collections: state.collections.map((collection) =>
+        collection.id === collectionId
+          ? {
+              ...collection,
+              images: collection.images.filter((image) => image.id !== imageId),
+            }
+          : collection,
+      ),
+    })),
+}));
+
+export default useMediaStore;
